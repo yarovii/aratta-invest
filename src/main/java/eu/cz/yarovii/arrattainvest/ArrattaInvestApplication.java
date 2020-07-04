5d6cd93c9240cfaf95d3ff8f96e17296406741be
Hlib Yarovyi <yarovhli@fit.cvut.cz>
2020-07-04 18:29:31 +0200

    Create pom

---
+++ src/main/java/eu/cz/yarovii/arrattainvest/ArrattaInvestApplication.java
@@ -0,0 +1,13 @@
+package eu.cz.yarovii.arrattainvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrattaInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrattaInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/cz/yarovii/arrattainvest/ArrattaInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.cz.yarovii.arrattainvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrattaInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
